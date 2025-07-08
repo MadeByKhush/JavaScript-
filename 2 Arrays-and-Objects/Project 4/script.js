0f2e9b2c9a435539d7d10dc900c47926a72c0ceb
MadeByKhush <khushsharmacollage@gmail.com>
2025-07-08 16:43:21 +0530

    used nesting in address

--- 2 Arrays-and-Objects/Project 4/script.js
+++ 2 Arrays-and-Objects/Project 4/script.js
@@ -4,7 +4,12 @@ const student = {
     Course: "BCA",
     age: 21,
     email: "rahulVerma@gmail.com",
-    address: "7/59 Gandhi Nagar (Ahemdabad)",
+    address: {
+        street: "7/59 Gandhi Nagar",
+        city: "Ahemdabad",
+        state: "Gujarat",
+        pincode: 380004
+    }
 };
 
 //get card container
@@ -15,5 +20,7 @@ cardDiv.innerHTML = `<h2><span id="para">${student.name}</span></h2>
 <p><strong>Course </strong>${student.Course}</p>
 <p><strong>Age </strong>${student.age}</p>
 <p><strong>Email </strong>${student.email}</p>
-<p><strong>Address </strong>${student.address}</p>
+  <p><strong>Address:</strong></p>
+  <p>${student.address.street}</p>
+  <p>${student.address.city}, ${student.address.state} - ${student.address.pincode}</p>
 `;
