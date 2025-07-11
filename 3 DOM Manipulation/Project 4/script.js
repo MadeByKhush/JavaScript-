2cf045e32c757d7af232eb12639fcff31a140aa1
MadeByKhush <khushsharmacollage@gmail.com>
2025-07-11 22:15:13 +0530

    changes perform in show/hide menu

--- 3 DOM Manipulation/Project 4/script.js
+++ 3 DOM Manipulation/Project 4/script.js
@@ -1,19 +1,7 @@
-// Select elements
-const input = document.getElementById('itemInput');
-const addBtn = document.getElementById('addBtn');
-const list = document.getElementById('itemList');
+const toggleBtn = document.getElementById("menu-toggle");
+const navLink = document.getElementById("nav-links");
 
-// Add event listener to button
-addBtn.addEventListener('click', function () {
-  const itemText = input.value.trim();
-
-  if (itemText !== '') {
-    const li = document.createElement('li');
-    li.textContent = itemText;
-    list.appendChild(li);
-    input.value = ''; // Clear input
-    input.focus();    // Focus back on input
-  } else {
-    alert("Please enter a valid item!");
-  }
-});
+toggleBtn.addEventListener("click", function() 
+{
+  navLink.classList.toggle("hidden");
+})
